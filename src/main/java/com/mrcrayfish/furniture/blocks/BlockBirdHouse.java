c739277503ffd73e739ed691a276929a27949bf7
ACGaming <4818419+ACGaming@users.noreply.github.com>
2025-11-14 14:11:22 +0100

    Streamline bird house

--- src/main/java/com/mrcrayfish/furniture/blocks/BlockBirdHouse.java
+++ src/main/java/com/mrcrayfish/furniture/blocks/BlockBirdHouse.java
@@ -1,19 +1,10 @@
 package com.mrcrayfish.furniture.blocks;
 
-import com.mrcrayfish.furniture.MrCrayfishFurnitureMod;
-import com.mrcrayfish.furniture.tileentity.TileEntityPlate;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockHorizontal;
-import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.PropertyDirection;
-import net.minecraft.block.state.BlockFaceShape;
-import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
@@ -22,32 +13,15 @@ import net.minecraft.world.World;
 
 import java.util.List;
 
-public class BlockBirdHouse extends Block implements ITileEntityProvider
+public class BlockBirdHouse extends BlockFurniture
 {
     private static final AxisAlignedBB BOUNDING_BOX = new AxisAlignedBB(1.0 / 16.0, 0.0 / 16.0, 1.0 / 16.0, 15.0 / 16.0, 20.0 / 16.0, 15.0 / 16.0);
-    public static final PropertyDirection FACING = BlockHorizontal.FACING;
 
     public BlockBirdHouse(Material material, String id)
     {
-        super(material);
+        super(material, id);
         this.setHardness(1F);
         this.setSoundType(SoundType.WOOD);
-        this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
-        this.setCreativeTab(MrCrayfishFurnitureMod.tabFurniture);
-        this.setTranslationKey(id);
-        this.setRegistryName(id);
-    }
-
-    @Override
-    public boolean isOpaqueCube(IBlockState state)
-    {
-        return false;
-    }
-
-    @Override
-    public boolean isFullCube(IBlockState state)
-    {
-        return false;
     }
 
     @Override
@@ -63,35 +37,8 @@ public class BlockBirdHouse extends Block implements ITileEntityProvider
     }
 
     @Override
-    public TileEntity createNewTileEntity(World worldIn, int meta)
+    public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
     {
-        return new TileEntityPlate();
-    }
-
-    @Override
-    public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face)
-    {
-        return BlockFaceShape.UNDEFINED;
-    }
-    @Override
-    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing,
-                                            float hitX, float hitY, float hitZ, int meta,
-                                            EntityLivingBase placer) {
         return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
-    }
-
-    @Override
-    public IBlockState getStateFromMeta(int meta) {
-        return this.getDefaultState().withProperty(FACING, EnumFacing.byHorizontalIndex(meta % 4));
-    }
-
-    @Override
-    public int getMetaFromState(IBlockState state) {
-        return state.getValue(FACING).getHorizontalIndex();
-    }
-
-    @Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, FACING);
     }
 }
