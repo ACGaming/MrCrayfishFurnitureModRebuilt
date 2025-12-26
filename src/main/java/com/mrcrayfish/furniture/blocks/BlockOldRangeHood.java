f83284dfddbf85f71fdbd47083924551c601cc9d
MisterIceCat <mistericecat@gmail.com>
2025-12-26 21:42:34 +0100

    Added the 1.7.10 Oven Range hood

---
+++ src/main/java/com/mrcrayfish/furniture/blocks/BlockOldRangeHood.java
@@ -0,0 +1,104 @@
+package com.mrcrayfish.furniture.blocks;
+
+import com.google.common.collect.Lists;
+import com.mrcrayfish.furniture.util.Bounds;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BlockOldRangeHood extends BlockFurniture
+{
+
+    public static final AxisAlignedBB[] COLLISION_VENTILATION = new Bounds(0.5, 0.0, 0.2, 1.0, 1.0, 0.8).getRotatedBounds();
+    public static final AxisAlignedBB COLLISION_BASE = new AxisAlignedBB(0, 0, 0, 1, 4 * 0.0625, 1);
+
+    public BlockOldRangeHood(Material material, String id)
+    {
+        super(material, id);
+        this.setHardness(0.5F);
+        this.setSoundType(SoundType.METAL);
+        this.setLightLevel(0.5F);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state)
+    {
+        return false;
+    }
+
+    protected List<AxisAlignedBB> getCollisionBoxList(IBlockState state, World world, BlockPos pos)
+    {
+        EnumFacing facing = state.getValue(FACING);
+        List<AxisAlignedBB> boxes = new ArrayList<>();
+        boxes.add(COLLISION_VENTILATION[facing.getHorizontalIndex()]);
+        boxes.add(COLLISION_BASE);
+        return boxes;
+    }
+
+    @Override
+    public void addCollisionBoxToList(IBlockState state, World world, BlockPos pos,
+                                      AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes,
+                                      Entity entityIn, boolean p_185477_7_)
+    {
+        List<AxisAlignedBB> list = getCollisionBoxList(state, world, pos);
+        for(AxisAlignedBB box : list)
+        {
+            addCollisionBoxToList(pos, entityBox, collidingBoxes, box);
+        }
+    }
+
+    @Override
+    public RayTraceResult collisionRayTrace(IBlockState blockState, World world,
+                                            BlockPos pos, Vec3d start, Vec3d end)
+    {
+        List<RayTraceResult> list = Lists.newArrayList();
+
+        for(AxisAlignedBB axisalignedbb : getCollisionBoxList(blockState, world, pos))
+        {
+            list.add(this.rayTrace(pos, start, end, axisalignedbb));
+        }
+
+        RayTraceResult raytraceresult1 = null;
+        double d1 = 0.0D;
+
+        for(RayTraceResult raytraceresult : list)
+        {
+            if(raytraceresult != null)
+            {
+                double d0 = raytraceresult.hitVec.squareDistanceTo(end);
+
+                if(d0 > d1)
+                {
+                    raytraceresult1 = raytraceresult;
+                    d1 = d0;
+                }
+            }
+        }
+
+        return raytraceresult1;
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState()
+    {
+        return new BlockStateContainer(this, FACING);
+    }
+}
--- src/main/java/com/mrcrayfish/furniture/init/FurnitureBlocks.java
+++ src/main/java/com/mrcrayfish/furniture/init/FurnitureBlocks.java
@@ -129,6 +129,7 @@ public class FurnitureBlocks
     public static final Block BLINDS_OLD;
     public static final Block BLINDS_OLD_CLOSED;
     public static final Block OVEN_OLD;
+    public static final Block RANGE_HOOD_OLD;
 
     static
     {
@@ -402,6 +403,7 @@ public class FurnitureBlocks
         BLINDS_OLD = new BlockBlinds(Material.WOOD, true, "blinds_open_old");
         BLINDS_OLD_CLOSED = new BlockBlinds(Material.WOOD, false, "blinds_closed_old");
         OVEN_OLD = new BlockOven(Material.ROCK, "oven_old");
+        RANGE_HOOD_OLD = new BlockOldRangeHood(Material.ROCK, "range_hood_old");
     }
 
     public static void register()
@@ -676,6 +678,7 @@ public class FurnitureBlocks
         registerBlock(BLINDS_OLD);
         registerBlock(BLINDS_OLD_CLOSED, null);
         registerBlock(OVEN_OLD);
+        registerBlock(RANGE_HOOD_OLD);
     }
 
     public static void registerBlock(Block block)
