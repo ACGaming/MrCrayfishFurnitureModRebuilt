900613ba423568c182a56df67ce1999e016adcb6
MisterIceCat <mistericecat@gmail.com>
2025-12-28 21:49:31 +0100

    A small fix for the Food Dispenser

--- src/main/java/com/mrcrayfish/furniture/blocks/BlockFoodDispenserCat.java
+++ src/main/java/com/mrcrayfish/furniture/blocks/BlockFoodDispenserCat.java
@@ -28,6 +28,7 @@ import java.util.Random;
 public class BlockFoodDispenserCat extends BlockFurniture
 {
     public static final PropertyInteger LEVEL = PropertyInteger.create("level", 0, 3);
+    private static final AxisAlignedBB BOUNDING_BOX = new AxisAlignedBB(0, 0, 0.125, 1, 1, 0.875);
 
     public BlockFoodDispenserCat(Material material, String id)
     {
@@ -160,5 +161,11 @@ public class BlockFoodDispenserCat extends BlockFurniture
     {
         tooltip.add("§6Work in progress, no GUI yet!");
         super.addInformation(stack, worldIn, tooltip, flagIn);
+    }
+
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        return BOUNDING_BOX;
     }
 }
