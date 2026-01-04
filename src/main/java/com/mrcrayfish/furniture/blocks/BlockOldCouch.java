7fd9a0befcb3dcaf46caff017c2d0d48cf990b37
MisterIceCat <mistericecat@gmail.com>
2026-01-04 23:21:41 +0100

    I added a special text for old furnitures
    It's a special text for old furnitures from 1.7.10 or 1.6.4

--- src/main/java/com/mrcrayfish/furniture/blocks/BlockOldCouch.java
+++ src/main/java/com/mrcrayfish/furniture/blocks/BlockOldCouch.java
@@ -1,5 +1,12 @@
 package com.mrcrayfish.furniture.blocks;
 
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
 public class BlockOldCouch extends BlockCouch
 {
     public BlockOldCouch(String id)
@@ -11,5 +18,13 @@ public class BlockOldCouch extends BlockCouch
     public boolean isSpecial()
     {
         return true;
+    }
+
+    private static final String[] COLORS = {"§c","§e","§a","§b","§d","§6"};
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
+    {
+        tooltip.add(COLORS[(int)((System.currentTimeMillis() / 500) % COLORS.length)] + "1.7.10 Furniture !");
     }
 }
