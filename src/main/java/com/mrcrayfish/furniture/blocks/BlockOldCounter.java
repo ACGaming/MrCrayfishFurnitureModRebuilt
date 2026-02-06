c47f06d8a60cc89dd69407aaba2c921721aae9e7
MisterIceCat <mistericecat@gmail.com>
2026-02-06 15:48:55 +0100

    Fix tooltip on Old Counter

--- src/main/java/com/mrcrayfish/furniture/blocks/BlockOldCounter.java
+++ src/main/java/com/mrcrayfish/furniture/blocks/BlockOldCounter.java
@@ -8,6 +8,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
@@ -171,6 +172,14 @@ public class BlockOldCounter extends BlockFurnitureTile
     public void getSubBlocks(CreativeTabs item, NonNullList<ItemStack> items)
     {
         items.add(new ItemStack(this));
+    }
+
+    private static final String[] COLORS = {"§c","§e","§a","§b","§d","§6"};
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
+    {
+        tooltip.add(COLORS[(int)((System.currentTimeMillis() / 500) % COLORS.length)] + "1.7.10 Furniture");
     }
 
     @Override
