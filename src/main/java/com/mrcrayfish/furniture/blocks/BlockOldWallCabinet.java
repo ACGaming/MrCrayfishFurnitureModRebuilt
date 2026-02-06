420d970eb29a60fa63c59e57ecf2cf137debffef
MisterIceCat <mistericecat@gmail.com>
2026-02-06 12:54:20 +0100

    🦔

--- src/main/java/com/mrcrayfish/furniture/blocks/BlockOldWallCabinet.java
+++ src/main/java/com/mrcrayfish/furniture/blocks/BlockOldWallCabinet.java
@@ -1,8 +1,16 @@
 package com.mrcrayfish.furniture.blocks;
 
+import com.mrcrayfish.furniture.MrCrayfishFurnitureMod;
+import com.mrcrayfish.furniture.tileentity.TileEntityOldWallCabinet;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
@@ -21,5 +29,32 @@ public class BlockOldWallCabinet extends BlockWallCabinet
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
     {
         tooltip.add(COLORS[(int)((System.currentTimeMillis() / 500) % COLORS.length)] + "1.6.4 Furniture");
+    }
+
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        if(!worldIn.isRemote)
+        {
+            TileEntity tile_entity = worldIn.getTileEntity(pos);
+
+            if(tile_entity instanceof TileEntityOldWallCabinet)
+            {
+                playerIn.openGui(MrCrayfishFurnitureMod.instance, 0, worldIn, pos.getX(), pos.getY(), pos.getZ());
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean hasTileEntity(IBlockState state)
+    {
+        return true;
+    }
+
+    @Override
+    public TileEntity createTileEntity(World world, IBlockState state)
+    {
+        return new TileEntityOldWallCabinet();
     }
 }
