91e62cf02a62bc3f057cceed91b8dd3a3b00ec07
MisterIceCat <mistericecat@gmail.com>
2026-02-06 15:28:21 +0100

    Fix Old Blender

--- src/main/java/com/mrcrayfish/furniture/blocks/BlockOldBlender.java
+++ src/main/java/com/mrcrayfish/furniture/blocks/BlockOldBlender.java
@@ -3,6 +3,7 @@ package com.mrcrayfish.furniture.blocks;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
@@ -21,5 +22,11 @@ public class BlockOldBlender extends BlockBlender
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
     {
         tooltip.add(COLORS[(int)((System.currentTimeMillis() / 500) % COLORS.length)] + "1.7.10 Furniture");
+    }
+
+    @Override
+    public BlockRenderLayer getRenderLayer()
+    {
+        return BlockRenderLayer.TRANSLUCENT;
     }
 }
