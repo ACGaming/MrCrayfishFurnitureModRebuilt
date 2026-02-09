d13b200b9659557d0f9d968f35be67e880187ba7
MisterIceCat <mistericecat@gmail.com>
2026-02-09 21:32:38 +0100

    Added tooltip on old counter sink 🦖

--- src/main/java/com/mrcrayfish/furniture/blocks/BlockOldCounterSink.java
+++ src/main/java/com/mrcrayfish/furniture/blocks/BlockOldCounterSink.java
@@ -8,6 +8,7 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
@@ -147,6 +148,14 @@ public class BlockOldCounterSink extends BlockFurnitureTile
                                   World world, BlockPos pos, EntityPlayer player)
     {
         return new ItemStack(this);
+    }
+
+    private static final String[] COLORS = {"§c","§e","§a","§b","§d","§6"};
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
+    {
+        tooltip.add(COLORS[(int)((System.currentTimeMillis() / 500) % COLORS.length)] + "1.7.10 Furniture");
     }
 
     @Nullable
