dbd50b831b84b050a0eec75f30b23a066ee005b3
MisterIceCat <mistericecat@gmail.com>
2026-05-05 16:18:57 +0200

    🎁

--- src/main/java/com/mrcrayfish/furniture/blocks/BlockFoodDispenserCat.java
+++ src/main/java/com/mrcrayfish/furniture/blocks/BlockFoodDispenserCat.java
@@ -1,7 +1,5 @@
 package com.mrcrayfish.furniture.blocks;
 
-import javax.annotation.Nullable;
-
 import com.mrcrayfish.furniture.init.FurnitureSounds;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
@@ -9,7 +7,6 @@ import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.passive.EntityOcelot;
 import net.minecraft.entity.player.EntityPlayer;
