cee1297587b9fd1a581c3b9d4d6338f87a150c9a
MisterIceCat <mistericecat@gmail.com>
2026-05-21 15:20:39 +0200

    Fix Old Lamp

--- src/main/java/com/mrcrayfish/furniture/blocks/BlockOldLamp.java
+++ src/main/java/com/mrcrayfish/furniture/blocks/BlockOldLamp.java
@@ -7,16 +7,19 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 import com.mrcrayfish.furniture.init.FurnitureBlocks;
 import java.util.List;
+import java.util.Random;
 
 public class BlockOldLamp extends BlockFurniture
 {
@@ -46,6 +49,18 @@ public class BlockOldLamp extends BlockFurniture
 		world.setBlockState(pos, FurnitureBlocks.LAMP_ON_OLD.getDefaultState().withProperty(FACING, state.getValue(FACING)));
 		return true;
 	}
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune)
+    {
+        return new ItemStack(FurnitureBlocks.LAMP_OFF_OLD).getItem();
+    }
+
+    @Override
+    public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player)
+    {
+        return new ItemStack(FurnitureBlocks.LAMP_OFF_OLD);
+    }
 
 	@Override
 	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
