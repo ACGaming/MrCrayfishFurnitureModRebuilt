78bafd7f2a6f59f18057a9ddb9a1c7907413b671
MisterIceCat <mistericecat@gmail.com>
2026-06-09 20:13:02 +0200

    Added a Solar Panel !

---
+++ src/main/java/com/mrcrayfish/furniture/blocks/BlockSolarPanel.java
@@ -0,0 +1,49 @@
+package com.mrcrayfish.furniture.blocks;
+
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class BlockSolarPanel extends BlockFurniture
+{
+    private static final AxisAlignedBB BOUNDING_BOX = new AxisAlignedBB(0.1, 0, 0.1, 0.9, 0.80, 0.9);
+
+    public BlockSolarPanel(Material material, String id)
+    {
+        super(material, id);
+        this.setHardness(1F);
+        this.setSoundType(SoundType.METAL);
+    }
+
+    @Override
+    public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+    {
+        return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing());
+    }
+
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        return BOUNDING_BOX;
+    }
+
+    private static final String[] COLORS = {"§c","§e","§a","§b","§d","§6"};
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
+    {
+        tooltip.add(COLORS[(int)((System.currentTimeMillis() / 500) % COLORS.length)] + "1.7.10 Furniture");
+        tooltip.add("§6Work in progress...");
+    }
+}
